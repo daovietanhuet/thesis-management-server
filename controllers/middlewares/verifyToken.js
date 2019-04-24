8a2853096a2e8e1f89591c371d35d0242e4b2046
daovietanhuet <daovietanhsky@gmail.com>
2019-04-24 10:49:11 +0700

    update verify token

--- controllers/middlewares/verifyToken.js
+++ controllers/middlewares/verifyToken.js
@@ -3,29 +3,29 @@ const {UsersRepository} = require('repositories');
 const {ErrorHandler} = require('libs');
 const jwt = require('jsonwebtoken');
 
-module.exports = (req, res, next) => {
+module.exports = async (req, res, next) => {
     const bearerHeader = req.headers['authorization'];
     if(typeof bearerHeader !== undefined){
         try{
             const bearer = bearerHeader.split(" ");
             const bearerToken = bearer[1];
             const decoded = jwt.decode(bearerToken);
-            UsersRepository.findOne({
+            let user = await UsersRepository.findOne({
                 where:{id:decoded.data.id},
                 attributes:['password','username']
-            })
-                .then (user => {
-                    jwt.verify(bearerToken, `@${user.username}@${user.password}@uetthesis`, (error, result)=>{
-                        if(result){
-                            req.userId = result.data.id
-                            req.userRole = result.data.role
-                            next()
-                        } else if (error) {
-                            next(ErrorHandler.generateError('permission denied', 401, 'PERMISSION DENIED'))
-                        }
-                    })
-                })
-                .catch (error => {throw error})
+            });
+            if(user) {
+                let result = await jwt.verify(bearerToken, `@${user.username}@${user.password}@uetthesis`);
+                if(result) {
+                    req.userId = result.data.id;
+                    req.userRole = result.data.role;
+                    next();
+                } else {
+                    throw ErrorHandler.generateError('permission denied', 401, 'PERMISSION DENIED')
+                }
+            } else {
+                throw ErrorHandler.generateError('permission denied', 401, 'PERMISSION DENIED')
+            }
         } catch (error) {
             throw ErrorHandler.generateError('permission denied', 401, 'PERMISSION DENIED')
         }
